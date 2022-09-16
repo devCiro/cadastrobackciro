68d73aefea4194905e6798f88c786a2c142fe010
CiroDev1987 <cirodev.junior@gmail.com>
2022-09-16 12:20:35 -0300

    first commit

---
+++ cadastrobackciro/src/main/java/com/cadastrobackciro/CadastroBackCiroApplication.java
@@ -0,0 +1,13 @@
+package com.cadastrobackciro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroBackCiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroBackCiroApplication.class, args);
+	}
+
+}
